0decb271bf10a68e40a65576ea2722216920fd5b
Joanna Sullivan <jsullivan@gapintelligence.com>
2017-10-12 09:19:22 -0700

    Blog Crawler!

---
+++ crawler.rb
@@ -0,0 +1,10 @@
+class Crawler
+	def say_hello (product_info)
+		puts "Product"
+		puts "price"
+		puts "129.99"
+	end
+end
+
+
+
---
+++ run.rb
@@ -0,0 +1,9 @@
+require './blog_post.rb'
+require './crawler.rb'
+
+crawler = Crawler.new
+crawler.say_hello(ARGV[0])
+
+
+
+
